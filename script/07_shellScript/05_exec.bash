0efac1c8b858300c0af16aba644908c6effe872a
RYA234 <103587568+RYA234@users.noreply.github.com>
2024-06-20 23:45:04 +0000

    シェルスクリプト　引数　実行方法

---
+++ script/07_shellScript/05_exec.bash
@@ -0,0 +1,6 @@
+# bash　引数の確認
+# docker exec -it centos_main  bash script/07_shellScript/05_exec.bash
+chmod 777 ./script/07_shellScript/05_01_testargs.bash
+./script/07_shellScript/05_01_testargs.bash arg1
+
+
